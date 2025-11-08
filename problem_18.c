b242ac0e286103c23043610858582ece551569f1
Lakshyap18 <json06038@gmail.com>
2025-11-08 22:59:15 +0530

    Add files via upload

---
+++ problem_18.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int am(int n){
+    int temp = n,ans = 0, len;
+    for(len = 0;temp;len++,temp/=10);
+    temp = n;
+    while(temp){
+        int pow = 1;
+        int lastdigit = temp%10;
+        temp /=10;
+        for(int x =1;x <=len;x++)
+        pow = pow*lastdigit;
+        ans +=pow;
+    }
+    return(n==ans);
+}
+int main()
+{
+    for(int x =1;x<= 10000;x++)
+    if(am(x))
+    printf("%d",x);
+    printf("\n");
+    return 0;
+}
